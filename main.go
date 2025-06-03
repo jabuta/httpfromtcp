2bb945f34479b5504021183d99803ac65b0f493a
felo <lfalvaz@gmail.com>
2025-06-03 14:53:15 -0500

    finished part 1

--- main.go
+++ main.go
@@ -16,25 +16,42 @@ func main() {
 	if err != nil {
 		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
 	}
-	defer f.Close()
-
 	fmt.Printf("reading data from %s\n", inputFilePath)
 	fmt.Print("==============================\n")
-	var allfile = ""
-	for {
-		b := make([]byte, 8, 8)
-		_, err := f.Read(b)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				break
-			} else {
-				log.Fatalf("encountered error reading: %s\n", err)
-			}
-		}
-		allfile += string(b)
-	}
-	lines := strings.Split(allfile, "\n")
-	for _, line := range lines {
+	linesChanel := getLinesChannel(f)
+	for line := range linesChanel {
 		fmt.Printf("read: %s\n", line)
 	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	linesChan := make(chan string)
+
+	go func() {
+		defer close(linesChan)
+		defer f.Close()
+		currentLine := ""
+		for {
+			buffer := make([]byte, 8, 8)
+			n, err := f.Read(buffer)
+			if err != nil {
+				if currentLine != "" {
+					linesChan <- currentLine
+					currentLine = ""
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+			}
+			str := string(buffer[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				linesChan <- currentLine + parts[i]
+				currentLine = ""
+			}
+			currentLine += parts[len(parts)-1]
+		}
+
+	}()
+	return linesChan
 }
